a73008fbe3a18d6eb54e6fd2ec7fcdeb0255b0df
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-10-16 11:11:47 -0300

    API functionality complete
    Generalized the api call so that it can be used across all the cards for all three category pages.

--- RAWG.js
+++ RAWG.js
@@ -1,34 +1,12 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  const gameTitle = document.getElementById("masterpiece1");
+  const gameCards = document.querySelectorAll(".gameCard");
   const apiKey = "7f48b16c6de34092abdc24716b96cdad";
-  var battlefield4ESRB = document.getElementById("battlefield4ESRB");
-  var battlefield4Metacritic = document.getElementById(
-    "battlefield4Metacritic"
-  );
-  var battlefield4Genres = document.getElementById("battlefield4Genres");
-  var battlefield4Platforms = document.getElementById("battlefield4Platforms");
-  console.log(battlefield4Platforms);
+  console.log("Hello! For one of our wow factors, notice how the ESRB, Genre, Platform, and Metacritic scores on the category pages are all dynamically generated with the results of an api call to a gaming database."); 
 
-  async function gameInfo(
-    gameTitle,
-    apiKey,
-    battlefield4ESRB,
-    battlefield4Metacritic,
-    battlefield4Platforms,
-    battlefield4Genres
-  ) {
-    if (!gameTitle) {
-      console.error("gameTitle element not found!");
-      return;
-    }
-    console.log(gameTitle);
-    const gameTitleText = gameTitle.textContent.trim();
-    var url =
-      "https://api.rawg.io/api/games?key=" +
-      apiKey +
-      "&search=" +
-      gameTitleText;
-    console.log(url);
+  async function gameInfo(card) {
+    const gameTitleElement = card.querySelector("h2");
+    gameTitleText = gameTitleElement.textContent.trim();
+    const url = `https://api.rawg.io/api/games?key=${apiKey}&search=${gameTitleText}`;
 
     try {
       const response = await fetch(url);
@@ -36,78 +14,60 @@ document.addEventListener("DOMContentLoaded", (event) => {
       console.log(gameData);
 
       // ESRB Rating
-      if (
-        gameData.results &&
-        gameData.results.length > 0 &&
-        gameData.results[0].esrb_rating
-      ) {
-        battlefield4ESRB.textContent =
-          "ESRB Rating: " + gameData.results[0].esrb_rating.name;
-      } else {
-        battlefield4ESRB.textContent = "ESRB: Rating not available";
-      }
+      if (gameData.results && gameData.results.length > 0) {
+        const game = gameData.results[0];
 
-      // Metacritic Score
-      if (
-        gameData.results &&
-        gameData.results.length > 0 &&
-        gameData.results[0].metacritic
-      ) {
-        battlefield4Metacritic.textContent =
-          "Metacritic Score: " + gameData.results[0].metacritic;
-      } else {
-        battlefield4Metacritic.textContent = "Metacritic: Score not available";
-      }
+        const esrb = card.querySelector(".esrb");
+        if (game.esrb_rating) {
+          esrb.textContent = "ESRB Rating: " + game.esrb_rating.name;
+        } else {
+          esrb.textContent = "ESRB: Rating not available";
+        }
 
-      // Platforms
-      console.log("battlefield4Platforms:", battlefield4Platforms);
-      if (
-        gameData.results &&
-        gameData.results.length > 0 &&
-        gameData.results[0].platforms
-      ) {
-        let platformNames = [];
-        gameData.results[0].platforms.forEach((platformData) => {
-          if (platformData.platform && platformData.platform.name) {
-            platformNames.push(platformData.platform.name);
-          }
-        });
-        let platformString = platformNames.join(", ");
-        battlefield4Platforms.textContent = "Platform(s): " + platformString;
-      } else {
-        battlefield4Metacritic.textContent =
-          "Platform(s): Platforms not available";
-      }
+        // Metacritic Score
+        const metacritic = card.querySelector(".metacritic");
+        if (game.metacritic) {
+          metacritic.textContent = "Metacritic Score: " + game.metacritic;
+        } else {
+          metacritic.textContent = "Metacritic Score: Unavailable";
+        }
 
-      // Genres
-      console.log("battlefield4Genres: ", battlefield4Genres);
-      if (
-        gameData.results &&
-        gameData.results.length > 0 &&
-        gameData.results[0].genres
-      ) {
-        let genreNames = [];
-        gameData.results[0].genres.forEach((genreData) => {
-          if (genreData.name) {
-            genreNames.push(genreData.name);
-          }
-        });
-        let genreString = genreNames.join(", ");
-        console.log("genre string: " + genreString);
-        battlefield4Genres.textContent = "Genre(s): " + genreString; // Returning nothing...
-      } else {
-        battlefield4Genres.textContent = "Genre(s): Genres not available";
+        // Platforms
+        const platforms = card.querySelector(".platforms");
+        if (game.platforms) {
+          let platformNames = [];
+          game.platforms.forEach((platformData) => {
+            if (platformData.platform && platformData.platform.name) {
+              platformNames.push(platformData.platform.name);
+            }
+          });
+          let platformString = platformNames.join(", ");
+          platforms.textContent = "Platform(s): " + platformString;
+        } else {
+          platforms.textContent = "Platform(s): Platforms not available";
+        }
+
+        // Genres
+        const genres = card.querySelector(".genres");
+        if (game.genres) {
+          let genreNames = [];
+          game.genres.forEach((genreData) => {
+            if (genreData.name) {
+              genreNames.push(genreData.name);
+            }
+          });
+          let genreString = genreNames.join(", ");
+          console.log("genre string: " + genreString);
+          genres.textContent = "Genre(s): " + genreString;
+        } else {
+          genres.textContent = "Genre(s): Genres not available";
+        }
       }
     } catch (error) {
       console.error("Fetch error", error);
     }
   }
-  gameInfo(
-    gameTitle,
-    apiKey,
-    battlefield4ESRB,
-    battlefield4Metacritic,
-    battlefield4Platforms,
-    battlefield4Genres
-  );
+  gameCards.forEach((card) => {
+    gameInfo(card);
+  });
 });
