1569b0ad2fa017edf79006701f7b42db4ef5e24f
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-10-16 10:15:13 -0300

    test fetches
    All fetches working for battlefield 4

--- RAWG.js
+++ RAWG.js
@@ -2,8 +2,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const gameTitle = document.getElementById("masterpiece1");
   const apiKey = "7f48b16c6de34092abdc24716b96cdad";
   var battlefield4ESRB = document.getElementById("battlefield4ESRB");
+  var battlefield4Metacritic = document.getElementById(
+    "battlefield4Metacritic"
+  );
+  var battlefield4Genres = document.getElementById("battlefield4Genres");
+  var battlefield4Platforms = document.getElementById("battlefield4Platforms");
+  console.log(battlefield4Platforms);
 
-  async function gameInfo(gameTitle, apiKey, battlefield4ESRB) {
+  async function gameInfo(
+    gameTitle,
+    apiKey,
+    battlefield4ESRB,
+    battlefield4Metacritic,
+    battlefield4Platforms,
+    battlefield4Genres
+  ) {
     if (!gameTitle) {
       console.error("gameTitle element not found!");
       return;
@@ -18,21 +31,83 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.log(url);
 
     try {
-    const response = await fetch(url);
-    const gameData = await response.json();
-    console.log(gameData);
+      const response = await fetch(url);
+      const gameData = await response.json();
+      console.log(gameData);
+
+      // ESRB Rating
       if (
         gameData.results &&
         gameData.results.length > 0 &&
         gameData.results[0].esrb_rating
       ) {
-        battlefield4ESRB.textContent = gameData.results[0].esrb_rating.name;
+        battlefield4ESRB.textContent =
+          "ESRB Rating: " + gameData.results[0].esrb_rating.name;
       } else {
-        battlefield4ESRB.textContent = "ESRB rating not available";
+        battlefield4ESRB.textContent = "ESRB: Rating not available";
+      }
+
+      // Metacritic Score
+      if (
+        gameData.results &&
+        gameData.results.length > 0 &&
+        gameData.results[0].metacritic
+      ) {
+        battlefield4Metacritic.textContent =
+          "Metacritic Score: " + gameData.results[0].metacritic;
+      } else {
+        battlefield4Metacritic.textContent = "Metacritic: Score not available";
+      }
+
+      // Platforms
+      console.log("battlefield4Platforms:", battlefield4Platforms);
+      if (
+        gameData.results &&
+        gameData.results.length > 0 &&
+        gameData.results[0].platforms
+      ) {
+        let platformNames = [];
+        gameData.results[0].platforms.forEach((platformData) => {
+          if (platformData.platform && platformData.platform.name) {
+            platformNames.push(platformData.platform.name);
+          }
+        });
+        let platformString = platformNames.join(", ");
+        battlefield4Platforms.textContent = "Platform(s): " + platformString;
+      } else {
+        battlefield4Metacritic.textContent =
+          "Platform(s): Platforms not available";
+      }
+
+      // Genres
+      console.log("battlefield4Genres: ", battlefield4Genres);
+      if (
+        gameData.results &&
+        gameData.results.length > 0 &&
+        gameData.results[0].genres
+      ) {
+        let genreNames = [];
+        gameData.results[0].genres.forEach((genreData) => {
+          if (genreData.name) {
+            genreNames.push(genreData.name);
+          }
+        });
+        let genreString = genreNames.join(", ");
+        console.log("genre string: " + genreString);
+        battlefield4Genres.textContent = "Genre(s): " + genreString; // Returning nothing...
+      } else {
+        battlefield4Genres.textContent = "Genre(s): Genres not available";
       }
     } catch (error) {
-        console.error("Fetch error", error);
+      console.error("Fetch error", error);
     }
   }
-  gameInfo(gameTitle, apiKey, battlefield4ESRB);
+  gameInfo(
+    gameTitle,
+    apiKey,
+    battlefield4ESRB,
+    battlefield4Metacritic,
+    battlefield4Platforms,
+    battlefield4Genres
+  );
 });
