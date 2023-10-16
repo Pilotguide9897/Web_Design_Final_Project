1a4db7df02fc7efb2ecd4a021ab7797517527625
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-10-16 09:12:55 -0300

    RAWG api test
    Added test of RAWG api

--- RAWG.js
+++ RAWG.js
@@ -1,10 +1,38 @@
-// const gameTitle = document.getElementById("")
-// gameTitle.textContent = 
+document.addEventListener("DOMContentLoaded", (event) => {
+  const gameTitle = document.getElementById("masterpiece1");
+  const apiKey = "7f48b16c6de34092abdc24716b96cdad";
+  var battlefield4ESRB = document.getElementById("battlefield4ESRB");
 
-const apiKey = "7f48b16c6de34092abdc24716b96cdad";
+  async function gameInfo(gameTitle, apiKey, battlefield4ESRB) {
+    if (!gameTitle) {
+      console.error("gameTitle element not found!");
+      return;
+    }
+    console.log(gameTitle);
+    const gameTitleText = gameTitle.textContent.trim();
+    var url =
+      "https://api.rawg.io/api/games?key=" +
+      apiKey +
+      "&search=" +
+      gameTitleText;
+    console.log(url);
 
-async function gameInfo() {
-    const response = await fetch ("https://api.rawg.io/api/games?key=" + apiKey + "&search=" + );
-    const gameData = await response.json;
+    try {
+    const response = await fetch(url);
+    const gameData = await response.json();
     console.log(gameData);
-}
+      if (
+        gameData.results &&
+        gameData.results.length > 0 &&
+        gameData.results[0].esrb_rating
+      ) {
+        battlefield4ESRB.textContent = gameData.results[0].esrb_rating.name;
+      } else {
+        battlefield4ESRB.textContent = "ESRB rating not available";
+      }
+    } catch (error) {
+        console.error("Fetch error", error);
+    }
+  }
+  gameInfo(gameTitle, apiKey, battlefield4ESRB);
+});
